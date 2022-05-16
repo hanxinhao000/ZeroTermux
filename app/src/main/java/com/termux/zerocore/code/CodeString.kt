d83c371d393cc43fcf7a609fc26ea106c3b4fe99
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-05-16 12:50:41 +0800

    修复换源Bug

--- app/src/main/java/com/termux/zerocore/code/CodeString.kt
+++ app/src/main/java/com/termux/zerocore/code/CodeString.kt
@@ -10,20 +10,14 @@ package com.termux.zerocore.code
 public object CodeString {
 
 
-    public val QH:String = "sed -i 's@^\\(deb.*stable main\\)$@#\\1\\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list && sed -i 's@^\\(deb.*games stable\\)$@#\\1\\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' \$PREFIX/etc/apt/sources.list.d/game.list && sed -i 's@^\\(deb.*science stable\\)$@#\\1\\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' \$PREFIX/etc/apt/sources.list.d/science.list && apt update && apt upgrade \n"
+    public val QH:String = "sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/apt/termux-main stable main@' $PREFIX/etc/apt/sources.list && apt update && apt upgrade \n"
     public val BJ:String = "sed -i 's@^\\(deb.*stable main\\)$@#\\1\\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list &&" +
-        "sed -i 's@^\\(deb.*games stable\\)$@#\\1\\ndeb https://mirrors.bfsu.edu.cn/termux/game-packages-24 games stable@' \$PREFIX/etc/apt/sources.list.d/game.list &&" +
-        "sed -i 's@^\\(deb.*science stable\\)$@#\\1\\ndeb https://mirrors.bfsu.edu.cn/termux/science-packages-24 science stable@' \$PREFIX/etc/apt/sources.list.d/science.list &&" +
         "apt update && apt upgrade \n"
-    public val NJU:String = "sed -i 's@^\\(deb.*stable main\\)\$@#\\1\\ndeb https://mirror.nju.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list &&" +
-        "sed -i 's@^\\(deb.*games stable\\)\$@#\\1\\ndeb https://mirror.nju.edu.cn/termux/game-packages-24 games stable@' \$PREFIX/etc/apt/sources.list.d/game.list &&" +
-        "sed -i 's@^\\(deb.*science stable\\)\$@#\\1\\ndeb https://mirror.nju.edu.cn/termux/science-packages-24 science stable@' \$PREFIX/etc/apt/sources.list.d/science.list &&" +
+    public val NJU:String = "sed -i 's@^\\(deb.*stable main\\)\$@#\\1\\ndeb https://mirror.nju.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list &&" + +
         "apt update && apt upgrade \n"
-    public val USTC:String = "sed -i 's@packages.termux.org@mirrors.ustc.edu.cn/termux@' \$PREFIX/etc/apt/sources.list &&" +
+    public val USTC:String = "sed -i 's@^\\(deb.*stable main\\)$@#\\1\\ndeb https://mirrors.ustc.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list &&" +
         "pkg up \n"
     public val HEB:String = "sed -i 's@^\\(deb.*stable main\\)\$@#\\1\\ndeb https://mirrors.hit.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list &&" +
-        "sed -i 's@^\\(deb.*games stable\\)\$@#\\1\\ndeb https://mirrors.hit.edu.cn/termux/game-packages-24 games stable@' \$PREFIX/etc/apt/sources.list.d/game.list &&" +
-        "sed -i 's@^\\(deb.*science stable\\)\$@#\\1\\ndeb https://mirrors.hit.edu.cn/termux/science-packages-24 science stable@' \$PREFIX/etc/apt/sources.list.d/science.list &&" +
         "apt update && apt upgrade \n"
     public val UpDate:String = "pkg update -y \n"
     public val runLinuxSh:String = "cd ~ && cd ~ && chmod 777 linux.sh && ./linux.sh \n"
