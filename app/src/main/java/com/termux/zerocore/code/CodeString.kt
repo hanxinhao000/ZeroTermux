534fdf20a8e1705bf0560413391a5964a7dc86d9
XINHAO_HAN <1500252978@qq.com>
2022-05-22 23:18:48 +0800

    修复换源问题

--- app/src/main/java/com/termux/zerocore/code/CodeString.kt
+++ app/src/main/java/com/termux/zerocore/code/CodeString.kt
@@ -10,12 +10,13 @@ package com.termux.zerocore.code
 public object CodeString {
 
 
-    public val QH:String = "sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/apt/termux-main stable main@' $PREFIX/etc/apt/sources.list && apt update && apt upgrade \n"
+    public val QH:String = "sed -i 's@^\\(deb.*stable main\\)$@#\\1\\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list && apt update && apt upgrade \n"
     public val BJ:String = "sed -i 's@^\\(deb.*stable main\\)$@#\\1\\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list &&" +
         "apt update && apt upgrade \n"
-    public val NJU:String = "sed -i 's@^\\(deb.*stable main\\)\$@#\\1\\ndeb https://mirror.nju.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list &&" + +
+    public val NJU:String = "sed -i 's@^\\(deb.*stable main\\)\$@#\\1\\ndeb https://mirror.nju.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list &&" +
+        "sed -i 's@^\\(deb.*science stable\\)\$@#\\1\\ndeb https://mirror.nju.edu.cn/termux/science-packages-24 science stable@' \$PREFIX/etc/apt/sources.list.d/science.list &&" +
         "apt update && apt upgrade \n"
-    public val USTC:String = "sed -i 's@^\\(deb.*stable main\\)$@#\\1\\ndeb https://mirrors.ustc.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list &&" +
+    public val USTC:String = "sed -i 's@packages.termux.org@mirrors.ustc.edu.cn/termux@' \$PREFIX/etc/apt/sources.list &&" +
         "pkg up \n"
     public val HEB:String = "sed -i 's@^\\(deb.*stable main\\)\$@#\\1\\ndeb https://mirrors.hit.edu.cn/termux/termux-packages-24 stable main@' \$PREFIX/etc/apt/sources.list &&" +
         "apt update && apt upgrade \n"
