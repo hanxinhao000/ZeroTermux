324f8908f8e3d181984b94c71028f1f93c886748
XINHAO_HAN <1500252978@qq.com>
2023-02-04 13:34:55 +0800

    更新 {修复雪花背景Random 报错}

--- app/src/main/java/com/termux/zerocore/view/xuehua/Random.java
+++ app/src/main/java/com/termux/zerocore/view/xuehua/Random.java
@@ -10,10 +10,16 @@ class Random {
     }
 
     public float getRandom(float upper) {
+        if (upper <= 0) {
+            upper = 1080;
+        }
         return RANDOM.nextFloat() * upper;
     }
 
     public int getRandom(int upper) {
+        if (upper <= 0) {
+            upper = 1080;
+        }
         return RANDOM.nextInt(upper);
     }
 
