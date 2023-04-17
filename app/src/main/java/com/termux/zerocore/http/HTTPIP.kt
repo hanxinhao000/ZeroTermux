a8a319f1438a33125297e213b566aab0c4ddbabe
XINHAO_HAN <1500252978@qq.com>
2023-04-17 19:40:25 +0800

    更新 {修复下载站跳转404}

--- app/src/main/java/com/termux/zerocore/http/HTTPIP.kt
+++ app/src/main/java/com/termux/zerocore/http/HTTPIP.kt
@@ -3,7 +3,7 @@ package com.termux.zerocore.http
 object HTTPIP {
 
 
-    public const val IP = "http://od.ixcmstudio.cn"
+    public const val IP = "https://od.ixcmstudio.cn"
     //还得脚本位置
     public const val QEMU_HAI = "$IP/"
     //论坛
