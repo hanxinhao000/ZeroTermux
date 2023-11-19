468eb7e0c492705658adb4db3eb35011e726b6ef
XINHAO_HAN <1500252978@qq.com>
2023-11-19 22:16:32 +0800

    更新 {修改脚本描述}

--- app/src/main/res/raw/termux_timer.sh
+++ app/src/main/res/raw/termux_timer.sh
@@ -1,5 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
-### 这是在termux运行的脚本,默认pwd位置:
+### 这是在ZeroTermux运行的脚本,默认pwd位置:
 ### /data/data/com.termux/files/home
 ### 也就是Home位置
+### 启用脚本请在 ZeroTermux -> 定时任务 -> "定时任务设置"页面当中设置
+
+##  在此文件中写入你的shell代码
+### 示例代码
 pkg install openssh
