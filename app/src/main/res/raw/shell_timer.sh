693eed7a5033ad9e5ed0a39eea0e3b436c87de5e
XINHAO_HAN <1500252978@qq.com>
2023-11-19 22:11:17 +0800

    更新 {新增纯Android shell 脚本}

---
+++ app/src/main/res/raw/shell_timer.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+#################ZeroTermux###########
+#           Shell 增加会话脚本         #
+######################################
+## 注意此脚本是在Android shell 当中执行的命令
+## 不是在 ZeroTermux环境当中执行的，切记！
+## 在此方法中写入你的shell代码
+zero_termux_shell_main() {
+
+}
