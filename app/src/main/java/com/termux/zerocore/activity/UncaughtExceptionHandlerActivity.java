e8f939e0c25b2485de21aa08df7029fd148dd463
hanxinhao <hanxinhao@google.com>
2026-01-26 09:33:18 +0800

    [Phone][ZeroTermux][Bug][Github(/)]：更新报错页面的联系方式，更新翻译     RootCause: /     Modify: /     update: #hanxinhao000     Known problem: /     ApplyTo: All

--- app/src/main/java/com/termux/zerocore/activity/UncaughtExceptionHandlerActivity.java
+++ app/src/main/java/com/termux/zerocore/activity/UncaughtExceptionHandlerActivity.java
@@ -2,11 +2,13 @@ package com.termux.zerocore.activity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.xh_lib.utils.UUtils;
 import com.termux.R;
+import com.termux.zerocore.config.ZTConstantConfig;
 
 public class UncaughtExceptionHandlerActivity extends AppCompatActivity {
 
@@ -14,14 +16,11 @@ public class UncaughtExceptionHandlerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uncaught_exception_handler);
-
-
         String error = getIntent().getStringExtra("error");
-
-
-        EditText viewById = findViewById(R.id.error_text);
-
-        viewById.setText(viewById.getText().toString() + "\nVersion:" + UUtils.getVersionName(UUtils.getContext()) + "\n\n" + error);
-
+        TextView errorText = findViewById(R.id.error_text);
+        errorText.setText(errorText.getText().toString()
+            + ZTConstantConfig.ContactInformation.ZT_QQ_GROUP
+            + "\nVersion:" + UUtils.getVersionName(UUtils.getContext())
+            + "\n\n" + error);
     }
 }
